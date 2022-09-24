94bf5b898b333dc3c1b1a6671b69dac4bb654ad3
Marco Valeri <marcovaleri@hotmail.it>
2022-09-24 11:10:23 +0100

    Set up core files

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,1 +1,5 @@
-<h1>Hello PHP, this is my first MVC framework</h1>
+<?php
+// Load Libraries
+require_once 'libraries/Core.php';
+require_once 'libraries/Controller.php';
+require_once 'libraries/Database.php';
