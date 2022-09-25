f33985b4e7ee447c32911107b08fc10848a24763
Marco Valeri <marcovaleri@hotmail.it>
2022-09-25 13:42:04 +0100

    Set up db class

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,3 +1,8 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <h1><?= $data['title']; ?></h1>
+<ul>
+    <?php foreach ($data['posts'] as $post) { ?>
+            <li><?= $post->title; ?></li>
+    <?php } ?>
+</ul>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
